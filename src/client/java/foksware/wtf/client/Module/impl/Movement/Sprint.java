eb0986be3911942fb1df8b79d9d1945aee8ea936
sHv825 <wer100uwu@gmail.com>
2026-07-13 02:18:16 +0300

    Тоже норм обнова буду дкооче переписал ивент бас и модуль чуть чуть добавил 1 строку и добавил сеттинги типо скорее свего буду дополнять ну пока что я хз

--- src/client/java/foksware/wtf/client/Module/impl/Movement/Sprint.java
+++ src/client/java/foksware/wtf/client/Module/impl/Movement/Sprint.java
@@ -2,6 +2,7 @@ package foksware.wtf.client.Module.impl.Movement;
 
 
 import foksware.wtf.client.Events.Event;
+import foksware.wtf.client.Events.Subscribe;
 import foksware.wtf.client.Events.impl.TickEvent;
 import foksware.wtf.client.FoksWareClient;
 import foksware.wtf.client.Module.api.Module;
@@ -12,29 +13,30 @@ import static foksware.wtf.client.Utils.Client.IMinecraft.mc;
 
 @ModuleAnnotation(name = "Sprint", desc = "Тест модуль чисто фокс", type = Movement)
 public class Sprint extends Module {
-    public Sprint() {
         //это затычка ебанная потом фиксану или ии фиксанет я хззз
         //UPD: переписал чуть чуть логику модуля теперь контсруктора нету короче
         //UPD2: айфоны хуйня и майкрософт хуйня ебанная
         //UDP3: клауд хуйня разрешите заход в россии тогда имба
         //UPD4: ммм пиздец я в ауххе 29 градусов я т ут как  вбане сука
         //UPD5: суки не дают в клауд зайти
-    }
 
     @Override
     public void onEnable() {
-        FoksWareClient.eventBus.subscribe(TickEvent.class, this::onTickEvent);
+        FoksWareClient.eventBus.register(this);
     }
 
     @Override
     public void onDisable() {
-        FoksWareClient.eventBus.unsubscribe(TickEvent.class, this::onTickEvent);
+        FoksWareClient.eventBus.unregister(this);
     }
 
-    private void onTickEvent(Event event) {
+    //все поменял нахуй
+
+    @Subscribe
+    public void onTick(TickEvent event) {
         if (mc.player == null) return;
 
-        if (mc.player.input.forwardImpulse > 0 && !mc.player.horizontalCollision) {
+        if (mc.player.input.leftImpulse > 0 && !mc.player.horizontalCollision) {
             mc.player.setSprinting(true);
         }
     }
